d4de1393344f6462a34070f617f45d3742f6b516
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-09 17:31:28 -0500

    Demonstrate basics of using node config package.

--- _jwtsimple.js
+++ _jwtsimple.js
@@ -1,19 +1,20 @@
 const jwt = require("jwt-simple");
+const config = require("config");
 
 // get token
 const getToken = () => {
   const payload = { id: 1, name: "Charles" };
 
-  // takes in a payload and a secret, which is normally stored in an env variable
-  const token = jwt.encode(payload, "secret");
+  // using config package, we can setup and pull global variables
+  const token = jwt.encode(payload, config.get("jwtSecret"));
 
   return { token };
 };
 
 // validate the token
 const validateToken = token => {
-  // takes in the token and a secret, again, normally stored in an env variable
-  const decoded = jwt.decode(token, "secret");
+  // using config package, we can setup and pull global variables
+  const decoded = jwt.decode(token, config.get("jwtSecret"));
 
   return decoded;
 };
