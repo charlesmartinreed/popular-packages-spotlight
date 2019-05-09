eedce1dada2461da812af53b3a34adde3ccebbb3
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-09 17:34:48 -0500

    Demonstrate basics of using uuid package.

--- _jwtsimple.js
+++ _jwtsimple.js
@@ -1,9 +1,11 @@
 const jwt = require("jwt-simple");
 const config = require("config");
+const uuid = require("uuid");
 
 // get token
 const getToken = () => {
-  const payload = { id: 1, name: "Charles" };
+  // generate a fresh id with UUID
+  const payload = { id: uuid.v4(), name: "Charles" };
 
   // using config package, we can setup and pull global variables
   const token = jwt.encode(payload, config.get("jwtSecret"));
